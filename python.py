a62f9ea4802ed29dc27f40f8b8c44f1efdaf16e2
佐藤　祐弥 <y.satou@satouyuuwatarunoMacBook-Air.local>
2022-01-24 13:04:17 +0900

    start def

---
+++ python.py
@@ -0,0 +1,16 @@
+# 関数
+
+def say_hello():
+    print("hello world")
+
+
+say_hello()
+
+# 引数、戻り値
+def sum(x, y):
+    return x + y
+
+num1 =  sum(30, 30)
+num2 =  sum(3, 3)
+print(num1)
+print(num2)
