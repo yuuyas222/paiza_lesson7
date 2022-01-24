9cbdb5a4caed7b73f259e6843cec8debe025271f
佐藤　祐弥 <y.satou@satouyuuwatarunoMacBook-Air.local>
2022-01-24 15:05:53 +0900

    finish lesson

--- python.py
+++ python.py
@@ -14,3 +14,25 @@ num1 =  sum(30, 30)
 num2 =  sum(3, 3)
 print(num1)
 print(num2)
+
+
+import random
+
+def attack(enemy):
+    print("勇者は" + enemy + "を攻撃した。")
+    hit = random.randint(1,10)
+    if hit < 6 :
+        print(enemy + "に" + str(hit) + "の攻撃を与えた")
+    else:
+        print("クリティカルヒット" + str(hit) + "のダメージを与えた")
+
+enemies = ["スライム","モンスター","ドラゴン"]
+for enemy in enemies:
+    attack(enemy)
+
+
+def introduce(greeting, *names):
+    for name in names:
+        print("私は" + name + "です。" + greeting)
+
+introduce("こんにちは","勇者","村人")
